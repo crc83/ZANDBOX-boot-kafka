181ce42612857837f9126f395705c77d08158b62
Serhii Belei <sergiy.beley@gmail.com>
2018-11-12 20:37:16 +0200

    VanilaKafka Publisher + antlr pharser

--- src/main/java/com/sbelei/kafka/kafkademo/KafkademoApplication.java
+++ src/main/java/com/sbelei/kafka/kafkademo/KafkademoApplication.java
@@ -5,13 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.util.concurrent.FailureCallback;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.SuccessCallback;
 
 @SpringBootApplication
 public class KafkademoApplication implements CommandLineRunner {
@@ -35,9 +30,9 @@ public class KafkademoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		for (int i = 0; i < 100; i++) {
-			send("topic1", "msg"+i);
-		}
+//		for (int i = 0; i < 100; i++) {
+//			send("topic1", "msg"+i);
+//		}
 		
 	}
 }
